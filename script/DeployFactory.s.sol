095acd428f7c200de2fc38b758d451e097b39921
preseeds-commitor <contacts.preseeds@gmail.com>
2024-10-02 15:01:12 +0700

    build(script): deploy testnet

---
+++ script/DeployFactory.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import "../src/Factory.sol";
+
+contract DeployFactoryScript is Script {
+    function setUp() public {}
+
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        Factory factory = new Factory(1 ether);
+        console.log("Factory deployed at: ", address(factory));
+
+        vm.stopBroadcast();
+    }
+}
